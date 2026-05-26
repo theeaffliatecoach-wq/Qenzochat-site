7754cc2e41d177caa50922047656a1b4c986569f
theeaffliatecoach-wq <theeaffliatecoach@gmail.com>
2026-05-26 15:19:29 +0300

    production ready build

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
-const isDev = process.env.NODE_ENV === 'development'
-
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: isDev,
-})
-
-module.exports = withPWA({
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
+  turbopack: {},
+}
 
-  allowedDevOrigins: ['192.168.100.58'],
-
-  webpack(config) {
-    return config
-  },
-})
+module.exports = nextConfig
